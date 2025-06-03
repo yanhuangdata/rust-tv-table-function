4c0e40a6874d0404a915022fa4e4c65e26c7661b
Colerar <233hbj@gmail.com>
2025-06-03 10:45:47 +0800

    setup C++ and release CI

--- cpp/tests/call_rust_test.cpp
+++ cpp/tests/call_rust_test.cpp
@@ -1,7 +1,6 @@
 #include "../zngur/generated.h"
 #include "rust_tv_table_function.h"
 
-#include <boost/algorithm/string/predicate.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_vector.hpp>
 
@@ -12,7 +11,7 @@ TEST_CASE("call rust") {
     const auto name = registry.name();
     const auto *ptr = name.as_ptr();
     const auto name_view = std::string_view{reinterpret_cast<const char *>(ptr), name.len()};
-    if (!boost::iequals(name_view, "addtotals")) {
+    if (name_view != "addtotals") {
       continue;
     }
     auto table_func = rust::crate::create_raw(registry, nullptr,
